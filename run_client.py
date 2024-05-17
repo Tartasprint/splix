c74d65874b4efc74ae97c2f31be66beb0adc8528
Tartasprint <tartasprint.dev@gmail.com>
2024-05-17 15:05:08 +0200

    client load last model

--- run_client.py
+++ run_client.py
@@ -1,3 +1,4 @@
 from client.env import Env
 from dqn.mine.model import create_model
-print(len(Env(create_model(),200,1,True).run()))
+from tensorflow.keras.models import load_model
+print(len(Env(load_model('./models/2x256.keras'),200,0,True).run()))
